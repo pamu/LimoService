672c0bb8089637b378f6fd8c7e9d31f760d4d012
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-05-03 00:39:26 +0530

    login/signup

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,11 @@
 package controllers
 
+import play.api.libs.json.{JsError, JsSuccess, JsPath, Reads}
 import play.api.mvc.{Action, Controller}
+import play.api.libs.functional.syntax._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import scala.concurrent.Future
 
 
 object Application extends Controller {
@@ -9,14 +14,26 @@ object Application extends Controller {
     Ok(views.html.index("Welcome to LimoServices."))
   }
 
-  case class UserInfo(email: String, password: String)
+  case class Info(email: String, password: String)
 
-  def login = Action { implicit request =>
-    Ok("")
+  implicit val reads: Reads[Info] = (
+    (JsPath \ "email").read[String] and
+      (JsPath \ "password").read[String]
+    )(Info.apply _)
+
+
+  def mLogin = Action.async(parse.json) { implicit request =>
+    request.body.validate[Info] match {
+      case success: JsSuccess[Info] => Future(Ok(""))
+      case error: JsError => Future(BadRequest)
+    }
   }
 
-  def signup = Action { implicit request =>
-    Ok("")
+  def mSignup = Action.async(parse.json) { implicit request =>
+    request.body.validate[Info] match {
+      case success: JsSuccess[Info] => Future(Ok(""))
+      case error: JsError => Future(BadRequest)
+    }
   }
 
  }
